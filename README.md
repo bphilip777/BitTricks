0df3dc93cd247517b23aa913d594661ff760a956
Brian <bphilip777@gmail.com>
2025-03-20 21:39:35 -0600

    Fixing issues with vectorized threaded tag parser

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # BitTricks
+
+- Implementing Bit Twiddling Hacks by Sean Eron Anderson
+  url: https://graphics.stanford.edu/~seander/bithacks.html
+
+- Implementing Hacker's Delight's algos
+
--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,43 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+const CHAR_BIT = 8; // # of bits per byte - assume 8
+
+export fn sign(comptime T: type, x: T) T {
+    switch (@typeInfo(@TypeOf(T))) {
+        .int => {},
+        else => @compileError("Incorrect type"),
+    }
+
+    if (@typeInfo(@TypeOf(T)).int.signedness == .unsigned) return 0;
+
+    return (x > 0) - (x < 0);
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+test "sign" {}
+
+export fn isOppSign(comptime T: type, x: T, y: T) bool {
+    return (x ^ y) < 0;
 }
+
+export fn intAbs(comptime T: type, x: T) T {
+    const mask = v >> @sizeOf(v) - CHAR_BIT;
+    return (x + mask) ^ mask;
+}
+
+export fn minBranchless(comptime T: type, x: T, y: T) T {
+    return y ^ ((x ^ y) & -(x < y));
+}
+
+export fn maxBranchless(comptime T: type, x: T, y: T) T {
+    return x ^ ((x ^ y) & -(x < y));
+}
+
+// unsigned int
+export fn isPow2(comptime T: type, x: T) bool {
+    return v and !(v & (v -% 1));
+}
+
+export fn reverse(comptime T: type, x: T) T {}
+
+test "Reverse Bits" {}
