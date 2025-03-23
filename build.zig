979e97c06cd765b6d55c472defd8742caa026b28
Brian <bphilip777@gmail.com>
2025-03-22 23:24:51 -0600

    Added ability to run unit tests

--- build.zig
+++ build.zig
@@ -4,33 +4,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("BitTricks", .{
-        .root_source_file = b.path("src/root.zig"),
+    const bittricks = b.addModule("BitTricks", .{
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // const bittricks = b.addModule("BitTricks", .{
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
+    const unit_tests = b.addTest(.{
+        .root_module = bittricks,
+    });
+    const runnable_unit_tests = b.addRunArtifact(unit_tests);
 
-    // const exe = b.addExecutable(options: ExecutableOptions)
-
-    // const lib = b.addLibrary(.{
-    //     .linkage = .static,
-    //     .name = "BitTricks",
-    //     .root_module = bittricks,
-    // });
-    // b.installArtifact(lib);
-
-    // const lib_unit_tests = b.addTest(.{
-    //     .root_module = bittricks,
-    // });
-    //
-    // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    //
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_lib_unit_tests.step);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&runnable_unit_tests.step);
 }
