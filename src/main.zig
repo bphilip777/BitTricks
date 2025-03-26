0a349e1a40b735ca74cfcddae4ee52e9e8107f3c
Brian <bphilip777@gmail.com>
2025-03-26 04:42:08 -0600

    Fixed bug with turn on bits bw pairs of bits and renamed some fns

--- src/main.zig
+++ src/main.zig
@@ -97,6 +97,18 @@ test "Max Branchless" {
     }
 }
 
+pub fn isMult(comptime T: type, x: T, div: T) bool {
+    return @mod(x, div) == 0;
+}
+
+test "Is Mult" {
+    const is_even = isMult(u16, 80, 2);
+    try std.testing.expect(is_even);
+
+    const is_odd = !isMult(u16, 81, 2);
+    try std.testing.expect(is_odd);
+}
+
 pub fn isPow2(comptime T: type, x: T) bool {
     switch (@typeInfo(T)) {
         .int => {},
@@ -233,72 +245,21 @@ test "Swap XOR" {
     try std.testing.expect(a == 50 and b == 100);
 }
 
-pub fn turnOnBitsBW2Bits(
-    comptime T: type,
-    value: T,
-    prev_carry: bool,
-) struct { mask: T, carry: bool } {
-    switch (@typeInfo(T)) {
-        .int => |int| switch (int.signedness) {
-            .signed => @compileError("T must be an unsigned int"),
-            else => {},
-        },
-        else => @compileError("T must be an unsigned int"),
-    }
-
-    var temp: T = @bitReverse(value);
-    var mask: T = value;
-
-    const max_bit: T = @bitSizeOf(T);
-
-    if (prev_carry) {
-        const first_bit = @ctz(temp);
-        temp ^= @as(T, 1) << @truncate(first_bit);
-        for (0..first_bit) |i| {
-            mask |= @as(T, 1) << @truncate(max_bit - i - 1);
-        }
-    }
-
-    var carry: bool = false;
-    while (temp > 0) {
-        const first_bit = @ctz(temp);
-        temp ^= @as(T, 1) << @truncate(first_bit);
-
-        const second_bit = @ctz(temp);
-        if (second_bit == max_bit) {
-            carry = true;
-        } else {
-            temp ^= @as(T, 1) << @truncate(second_bit);
-        }
-
-        for (first_bit..second_bit) |i| {
-            mask |= @as(T, 1) << @truncate(max_bit - i - 1);
-        }
-    }
-
-    return .{
-        .mask = mask,
-        .carry = carry,
+pub fn createMask(comptime T: type) type {
+    return struct {
+        mask: T,
+        carry: bool,
     };
 }
 
-test "turnOnBitsBW2Bits" {
-    const T: type = u8;
-    const input_masks = [_]T{ 8, 66, 66, 74, 0, 255, 255 };
-    const input_carries = [_]bool{ false, false, true, true, true, false, true };
-    const expected_masks = [_]T{ 15, 126, 195, 206, 255, 255, 255 };
-    const expected_carries = [_]bool{ true, false, true, false, true, false, true };
+pub fn turnOnAllBits(comptime T: type) T {
+    return @as(T, 0) -% @as(T, 1);
+}
 
-    for (input_masks, input_carries, expected_masks, expected_carries, 0..) |imask, icarry, emask, ecarry, i| {
-        _ = i;
-        const mask, const carry = blk: {
-            const mask = turnOnBitsBW2Bits(T, imask, icarry);
-            break :blk .{ mask.mask, mask.carry };
-        };
-        try std.testing.expect(mask == emask and carry == ecarry);
-    }
-
-    try std.testing.expect(true);
+test "Turn On All Bits" {
+    const a = turnOnAllBits(u64);
+    const b = std.math.maxInt(u64);
+    try std.testing.expect(a == b);
 }
 
 fn doubleType(comptime T: type) type {
@@ -350,5 +311,268 @@ test "Turn Off Bit" {
     for (inputs, expected_answers) |input, expected_answer| {
         const answer = turnOffLastBit(u16, input);
         try std.testing.expect(answer == expected_answer);
+    }
+}
+
+pub fn indexOfRightmostBit(comptime T: type, n: T) T {
+    switch (@typeInfo(T)) {
+        .int => |int| {
+            switch (int.signedness) {
+                .signed => @compileError("Fn only accepts unsigned ints."),
+                .unsigned => {},
+            }
+        },
+        else => @compileError("Fn only accepts unsigned ints."),
+    }
+    if (n == 0) return 0;
+    return @ctz(n) + 1;
+}
+
+test "Index Of Rightmost Bit" {
+    const inputs = [_]u16{ 0b1010, 0b10, 0b1000 };
+    const expected_answers = [_]u16{ 0b10, 0b10, 0b0100 };
+    for (inputs, expected_answers) |input, expected_answer| {
+        const answer = indexOfRightmostBit(u16, input);
+        try std.testing.expect(answer == expected_answer);
+    }
+}
+
+pub fn turnOnIndex(comptime T: type, x: T, idx: T) T {
+    switch (@typeInfo(T)) {
+        .int => |int| {
+            switch (int.signedness) {
+                .signed => @compileError("Fn only accepts unsigned ints."),
+                .unsigned => {},
+            }
+        },
+        else => @compileError("Fn only accepts unsigned ints."),
+    }
+    return x | @as(T, 1) << @truncate(idx);
+}
+
+test "Turn On Index" {
+    // zero-idxed
+    const inputs = [_]u16{ 0, 1, 2, 3, 4 };
+    const expected_answers = [_]u16{ 1, 2, 4, 8, 16 };
+    for (inputs, expected_answers) |input, expected_answer| {
+        const answer = turnOnIndex(u16, 0, input);
+        try std.testing.expect(answer == expected_answer);
+    }
+}
+
+pub fn turnOnBitsBWPairsOfBits(
+    comptime T: type,
+    mask: createMask(T),
+) createMask(T) {
+    // Default: turns on bits b/w every 2 bits, default start is first bit to second bit
+    // Previous Carry = tells fn to turn on 0 to first flipped bit
+    // Value = mask containing flipped bits
+    // Cases:
+    // 1. Mask = All 0's, Carry = True -> Mask = All 1's, Carry = True, regardless of # of bits
+    // 2. Mask = All 1's, Carry = True -> Depends on # of turned on bits
+    // 3. Mask = Mixed, Carry = True -> Depends on # of Bits
+
+    // 4. Mask = 0, Carry = False -> Mask = 0, Carry = False, regardless of # of bits
+    // 5. Mask = 1's, Carry = False -> Depends on # of turned on bits
+    // 6. Mask = Mixed, Carry = False -> Depends on # of turned on Bits
+    switch (@typeInfo(T)) {
+        .int => |int| switch (int.signedness) {
+            .signed => @compileError("T must be an unsigned int"),
+            .unsigned => if (int.bits > 255) @compileError("T is too large, currently made to support up to but not including u256"),
+        },
+        else => @compileError("T must be an unsigned int"),
+    }
+
+    var temp: T = @bitReverse(mask.mask);
+
+    var new_mask: createMask(T) = .{
+        .mask = 0,
+        .carry = false,
+    };
+
+    const max_bit: T = @bitSizeOf(T);
+
+    if (mask.carry) {
+        const first_bit: T = 0;
+
+        const second_bit: T = @ctz(temp);
+        if (second_bit == max_bit) {
+            new_mask.mask = ~new_mask.mask;
+            new_mask.carry = true;
+            return new_mask;
+        }
+        temp = turnOffLastBit(T, temp);
+
+        // end inclusive
+        for (first_bit..second_bit + 1) |i| {
+            new_mask.mask |= @as(T, 1) << @truncate(max_bit - i - 1);
+        }
+    }
+
+    while (temp > 0) {
+        const first_bit = @ctz(temp);
+        temp = turnOffLastBit(T, temp);
+
+        const second_bit = @ctz(temp);
+        if (second_bit == max_bit) {
+            // end not inclusive
+            for (first_bit..second_bit) |i| {
+                new_mask.mask |= @as(T, 1) << @truncate(max_bit - i - 1);
+            }
+            new_mask.carry = true;
+            return new_mask;
+        }
+        temp = turnOffLastBit(T, temp);
+
+        // end inclusive
+        for (first_bit..second_bit + 1) |i| {
+            new_mask.mask |= @as(T, 1) << @truncate(max_bit - i - 1);
+        }
+    }
+
+    return new_mask;
+}
+
+test "turnOnBitsBWPairsOfBits" {
+    // Cases:
+    // 1-3: Carry = True, 4-6 Carry = False;
+    // # of bits
+    // 1 + 4 = 0s, 2 + 5 = 1s, 3 + 6 = Mixed
+
+    // 1. Mask = All 0's, Carry = True -> Mask = All 1's, Carry = True, regardless of # of bits
+    {
+        // 1a. Even # of bits
+        const T1: type = u8;
+        const mask1: createMask(T1) = .{
+            .mask = 0,
+            .carry = true,
+        };
+        const new_mask1 = turnOnBitsBWPairsOfBits(T1, mask1);
+        try std.testing.expect(new_mask1.mask == std.math.maxInt(T1));
+        try std.testing.expect(new_mask1.carry == true);
+
+        // 1b. Odd # of bits
+        const T2: type = u7;
+        const mask2: createMask(T2) = .{
+            .mask = 0,
+            .carry = true,
+        };
+        const new_mask2 = turnOnBitsBWPairsOfBits(T2, mask2);
+        try std.testing.expect(new_mask2.mask == std.math.maxInt(T2));
+        try std.testing.expect(new_mask2.carry == true);
+    }
+
+    // 2. Mask = All 1's, Carry = True ->
+    {
+        // 2a. Even # of Bits -> All 1's, Carry = True,
+        const T1: type = u8;
+        const mask1: createMask(T1) = .{
+            .mask = turnOnAllBits(T1),
+            .carry = true,
+        };
+        const new_mask1 = turnOnBitsBWPairsOfBits(T1, mask1);
+        try std.testing.expect(new_mask1.mask == std.math.maxInt(T1));
+        try std.testing.expect(new_mask1.carry == true);
+
+        // 2b. Odd # of Bits -> All 1's, Carry = False,
+        const T2: type = u7;
+        const mask2: createMask(T2) = .{
+            .mask = turnOnAllBits(T2),
+            .carry = true,
+        };
+        const new_mask2 = turnOnBitsBWPairsOfBits(T2, mask2);
+        try std.testing.expect(new_mask2.mask == std.math.maxInt(T2));
+        try std.testing.expect(new_mask2.carry == false);
+    }
+
+    // 3. Mask = Mixed, Carry = True ->
+    {
+        // 3a. Even # of Bits Turned On -> Mask = 0-first bit on + succeeding pairs are on; carry = true
+        // 010010010010 -> 110011110011
+        const T1: type = u16;
+        const mask1: createMask(T1) = .{
+            .mask = 0b0000_0000_1010_1010,
+            .carry = true,
+        };
+        const new_mask1 = turnOnBitsBWPairsOfBits(T1, mask1);
+        try std.testing.expect(new_mask1.mask == 0b1111111110111011);
+        try std.testing.expect(new_mask1.carry == true);
+
+        // 3b. Odd # of Bits Turned On -> Mask = 0-first bit on + succeeding pairs are on; carry  = false;
+        // 000010010010 -> 000011110011
+        const T2: type = u16;
+        const mask2: createMask(T2) = .{
+            .mask = 0b0100_0000_1001_0000,
+            .carry = true,
+        };
+        const new_mask2 = turnOnBitsBWPairsOfBits(T2, mask2);
+        try std.testing.expect(new_mask2.mask == 0b1100000011110000);
+        try std.testing.expect(new_mask2.carry == false);
+    }
+
+    // 4. Mask = 0, Carry = False -> Mask = 0, Carry = False, regardless of # of bits
+    {
+        // 4a. Even # of bits
+        const T1: type = u8;
+        const mask1: createMask(T1) = .{
+            .mask = 0,
+            .carry = false,
+        };
+        const new_mask1 = turnOnBitsBWPairsOfBits(T1, mask1);
+        try std.testing.expect(new_mask1.mask == 0 and new_mask1.carry == false);
+
+        // 4b. Odd # of Bits
+        const T2: type = u7;
+        const mask2: createMask(T2) = .{
+            .mask = 0,
+            .carry = false,
+        };
+        const new_mask2 = turnOnBitsBWPairsOfBits(T2, mask2);
+        try std.testing.expect(new_mask2.mask == 0 and new_mask2.carry == false);
+    }
+
+    // 5. Mask = 1's, Carry = False
+    {
+        // 5a. Even # of Bits -> Mask = All 1's, Carry = False
+        const T1: type = u8;
+        const mask1: createMask(T1) = .{
+            .mask = turnOnAllBits(T1),
+            .carry = false,
+        };
+        const new_mask1 = turnOnBitsBWPairsOfBits(T1, mask1);
+        try std.testing.expect(new_mask1.mask == turnOnAllBits(T1) and new_mask1.carry == false);
+
+        // 5b. Odd # of Bits -> Mask = All 1's, Carry = True
+        const T2: type = u7;
+        const mask2: createMask(T2) = .{
+            .mask = turnOnAllBits(T2),
+            .carry = false,
+        };
+        const new_mask2 = turnOnBitsBWPairsOfBits(T2, mask2);
+        try std.testing.expect(new_mask2.mask == turnOnAllBits(T2) and new_mask2.carry == true);
+    }
+
+    // 6. Mask = Mixed, Carry = False
+    {
+        // 6a. Even # of Bits Turned On -> Mask = Every Other Pairs filled in, Carry = False
+        const T1: type = u16;
+        const mask1: createMask(T1) = .{
+            .mask = 0b0000_0010_1001_0010,
+            .carry = false,
+        };
+        const new_mask1 = turnOnBitsBWPairsOfBits(T1, mask1);
+        try std.testing.expect(new_mask1.mask == 0b0000_0011_1001_1110);
+        try std.testing.expect(new_mask1.carry == false);
+
+        // 6b. Odd # of Bits Turned On -> Mask = Every other Pair filled in, Carry = True
+        // 000010010010 -> 111110011110
+        const T2: type = u16;
+        const mask2: createMask(T2) = .{
+            .mask = 0b0000_0000_1001_0010,
+            .carry = false,
+        };
+        const new_mask2 = turnOnBitsBWPairsOfBits(T2, mask2);
+        try std.testing.expect(new_mask2.mask == 0b0000_0000_1111_0011);
+        try std.testing.expect(new_mask2.carry == true);
     }
 }
